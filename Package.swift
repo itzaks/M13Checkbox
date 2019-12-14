4eb86f6e5b14b36ce252c506e2225eb08a3d9f40
Yaroslav <yarodevuci@users.noreply.github.com>
2019-12-13 16:31:35 -0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -9,5 +9,13 @@
 import PackageDescription
 
 let package = Package(
-    name: "M13Checkbox"
+    name: "M13Checkbox",
+    products: [
+        .library(name: "M13Checkbox", targets: ["M13Checkbox"]),
+    ],
+    dependencies: [
+    ],
+    targets: [
+        .target(name: "M13Checkbox", dependencies: [], path: "Sources")
+    ]
 )
