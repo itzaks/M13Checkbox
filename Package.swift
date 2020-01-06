9d9353290a966024634dbb9500d2d4887dffac7d
Isak ✏ ⇝ <isak.burstrom@gmail.com>
2020-01-06 23:55:22 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,10 +1,4 @@
-//
-//  Package.swift
-//  M13Checkbox
-//
-//  Created by McQuilkin, Brandon on 4/13/16.
-//  Copyright © 2016 Brandon McQuilkin. All rights reserved.
-//
+// swift-tools-version:5.1
 
 import PackageDescription
 
